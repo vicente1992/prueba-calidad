6303cb7278da8caaefe4f5b53235bccc838c36e0
vicente1992 <vic_ortiz20@hotmail.es>
2022-11-15 20:51:26 -0500

    Cambios de la rama master

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 app.use('/api/1.0', (req, res) => {
+  console.log('Cambios raealizados por la rama master');
   res.send({ mesage: 'Hola Mundo!' })
 })
 
